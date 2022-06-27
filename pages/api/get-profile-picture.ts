b1bc49cf07dd35f928c61be152c353d3c99c27a0
Amel <53909124+ADEV-00@users.noreply.github.com>
2022-06-27 16:55:44 +0000

    Finished with backend

---
+++ pages/api/get-profile-picture.ts
@@ -0,0 +1,78 @@
+import chromium from "chrome-aws-lambda";
+
+async function getBrowserInstance() {
+  const executablePath = await chromium.executablePath;
+
+  if (!executablePath) {
+    // running locally
+    const puppeteer = require("puppeteer");
+    return puppeteer.launch({
+      args: chromium.args,
+      headless: true,
+      defaultViewport: {
+        width: 1280,
+        height: 720,
+      },
+      ignoreHTTPSErrors: true,
+    });
+  }
+
+  return chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: {
+      width: 1280,
+      height: 720,
+    },
+    executablePath,
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true,
+  });
+}
+
+export default async (req: any, res: any) => {
+  const url = req.body.url;
+
+  // Perform URL validation
+  if (!url || !url.trim()) {
+    res.json({
+      status: "error",
+      error: "Enter a valid URL",
+    });
+
+    return;
+  }
+
+  let browser = null;
+
+  try {
+    browser = await getBrowserInstance();
+    let page = await browser.newPage();
+    await page.goto(url);
+    await page.waitForSelector(".info-content");
+    const imgs = await page.$(".info-content > .avatar > .img-face");
+
+    const src = await imgs.getProperty("src");
+    const image = await src.jsonValue();
+
+    //change image resolution
+    const imageUrl = image.replace("/64", "/1280");
+    const imageUrl2 = imageUrl.replace("/64", "/720");
+
+    res.json({
+      status: "success",
+      image: imageUrl2,
+    });
+    // upload this buffer on AWS S3
+  } catch (error: any) {
+    console.log(error);
+    res.json({
+      status: "error",
+      data: error.message || "Something went wrong",
+    });
+    // return callback(error);
+  } finally {
+    if (browser !== null) {
+      await browser.close();
+    }
+  }
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,26 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import React, { useState } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  const [profileImage, setProfileImage] = useState(null as any);
+
+  const handleSubmitPorfile = async () => {
+    const res = await fetch("/api/get-profile-picture", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        url: "https://trovo.live/s/BotRix",
+      }),
+    }).then((res) => res.json());
+
+    setProfileImage(res.image);
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -16,9 +33,12 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <button onClick={() => handleGetProfilePicture()}>
+          Submit Picture
+        </button>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
 
@@ -59,14 +79,14 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
